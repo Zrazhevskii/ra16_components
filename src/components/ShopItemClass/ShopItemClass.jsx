31aecf186df28fe0150a8c6b8d51ecd01bb0d81c
Sanshai <vbn-134@yandex.ru>
2023-10-13 15:54:01 +0300

    добавление image, создание структуры

---
+++ src/components/ShopItemClass/ShopItemClass.jsx
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+
+const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull:
+        "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
+    price: 399,
+    currency: '£',
+};
+
+export default class ShopItemClass extends Component {
+    render() {
+        return (
+        <article>
+            ShopItemClass
+        </article>
+        )
+    }
+}
